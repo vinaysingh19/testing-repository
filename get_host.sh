d8ad5f7528c149dd32975336a593f574c64c7eef
Vinay Singh <vinay.singh@peopleinteractive.in>
2020-01-02 23:31:02 +0530

    created script file

--- get_host.sh
+++ get_host.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 AWSD=$(command -v aws)
 
-$AWSD --profile shaadiprod --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --filters "Name=tag:Name,Values=Photo Automation Crop Worker Spot" "Name=instance-state-name,Values=running" --output text >> /tmp/host.txt 
+$AWSD --profile shaadiprod --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --filters "Name=tag:Name,Values=Photo Automation Crop Worker Spot" "Name=instance-state-name,Values=running" --output text > /tmp/host.txt 
 
 
 for host in $(cat /tmp/host.txt)
 do
-       ssh -i internal.pem -q -o ConnectTimeout=3 ubuntu@"$host" "hostname"
+       ssh -i /home/Vinay/internal.pem -q -o ConnectTimeout=3 ubuntu@"$host" "hostname"
        if [ $? != 0 ]; then
-       $AWSD --profile shaadiprod --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filter "Name=private-ip-address,Values=$host"
+       $AWSD --profile shaadiprod --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filter "Name=private-ip-address,Values=$host" > /tmp/id.txt
        fi
 done 
