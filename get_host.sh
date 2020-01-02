f4ad48e0eb2eedc7783b1e5e04107be045086b9d
Vinay Singh <vinay.singh@peopleinteractive.in>
2020-01-02 23:27:01 +0530

    created script file

--- get_host.sh
+++ get_host.sh
@@ -2,3 +2,12 @@
 AWSD=$(command -v aws)
 
 $AWSD --profile shaadiprod --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --filters "Name=tag:Name,Values=Photo Automation Crop Worker Spot" "Name=instance-state-name,Values=running" --output text >> /tmp/host.txt 
+
+
+for host in $(cat /tmp/host.txt)
+do
+       ssh -i internal.pem -q -o ConnectTimeout=3 ubuntu@"$host" "hostname"
+       if [ $? != 0 ]; then
+       $AWSD --profile shaadiprod --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filter "Name=private-ip-address,Values=$host"
+       fi
+done 
