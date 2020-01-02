9341a95c2b348fb2adb66ef703ea35f48f823f1f
Vinay Singh <vinay.singh@peopleinteractive.in>
2020-01-02 23:41:50 +0530

    created script file

--- get_host.sh
+++ get_host.sh
@@ -8,6 +8,12 @@ for host in $(cat /tmp/host.txt)
 do
        ssh -i /home/Vinay/internal.pem -q -o ConnectTimeout=3 ubuntu@"$host" "hostname"
        if [ $? != 0 ]; then
-       $AWSD --profile shaadiprod --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filter "Name=private-ip-address,Values=$host" --output text > /tmp/id.txt
+       $AWSD --profile shaadiprod --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filter "Name=private-ip-address,Values=$host" --output text > /tmp/id.txt 
        fi
 done 
+
+for id in $(cat /tmp/id.txt)
+do
+	echo "$AWSD --profile shaadiprod --region us-east-1 ec2 terminate-instances --instance-ids $id"
+
+done
